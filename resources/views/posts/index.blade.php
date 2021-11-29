f2ebd184503403c5e5c30420fe86b783608942e4
Luiz Antonio <luiz8907@hotmail.com>
2021-11-29 17:56:06 -0300

    listagem de imagens ok

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,12 +2,19 @@
 
     <div class="container bg-green-500">
 
-        @foreach ($posts as $post)
-            <article>
-                {{ Storage::url($post->image->url) }}
-            </article>
-        @endforeach
+        <div class="grid grid-cols-3 gap-6">
 
+            @foreach ($posts as $post)
+                <article class="w-full h-80 bg-cover bg-center" style="background-image: url({{ url('storage/' . $post->image->url) }} )">
+
+                    {{-- <img src=" {{ Storage::url($post->image->url)  }}" alt=""> --}}
+                    {{-- <img src=" {{ url('storage/' . $post->image->url) }}" alt=""> --}}
+
+
+                </article>
+            @endforeach
+
+        </div>
 
     </div>
 
