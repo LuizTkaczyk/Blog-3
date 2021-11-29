5afc56cc0aa2d5f4f97f320077cd76a22ab4e1c5
Luiz Antonio <luiz8907@hotmail.com>
2021-11-29 18:07:57 -0300

    Tags erro

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,11 +1,32 @@
 <x-app-layout>
 
-    <div class="container bg-green-500">
+    <div class="container py-8">
 
         <div class="grid grid-cols-3 gap-6">
 
             @foreach ($posts as $post)
-                <article class="w-full h-80 bg-cover bg-center" style="background-image: url({{ url('storage/' . $post->image->url) }} )">
+                <article class="w-full h-80 bg-cover bg-center @if($loop->first) col-span-2 @endif"
+                    style="background-image: url({{ url('storage/' . $post->image->url) }} )">
+
+                    <div>
+                        @foreach ($post->tags as $tag)
+                            <a href="">{{$tag->name}}</a>
+                        @endforeach
+
+                        {{$post->tags}}
+                    </div>
+
+                    <div class="w-full h-80 flex flex-col justify-center">
+                        <h1 class="text-4xl text-white leading-8 font-bold">
+                            <a href="">
+                                {{$post->name}}
+                            </a>
+                        </h1>
+
+
+                    </div>
+
+
 
                     {{-- <img src=" {{ Storage::url($post->image->url)  }}" alt=""> --}}
                     {{-- <img src=" {{ url('storage/' . $post->image->url) }}" alt=""> --}}
